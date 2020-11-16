7129995a63359956f9f54233b7505e840f551741
Bastien Saidi <saidi.ireland@gmail.com>
2020-11-16 15:59:31 +0000

    Fixed License

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #MIT License
 
-#Copyright (c) 2020 Bastien Saidi - <bsaidi125079@instituteofeducation.ie>
+#Copyright (c) 2020 Bastien Saidi
 
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
